46679a3b8e7fb8d813042532d8b1ff8232e0d0db
sala7khaled <sala7khalad@gmail.com>
2026-02-17 14:06:38 +0200

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Salah Khaled on 17/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
